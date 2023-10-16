f20f263a34b27536835d96dba133c8b37357bf4b
godwinrob <rob@robgodwin.com>
2023-10-16 15:06:55 -0400

    add CORS middleware to router

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use axum::{
     Router,
 };
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
 
-    // tracing
+    // add tracing
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG").unwrap_or_else(|_| "rust_api_playground=debug".into()),
@@ -15,9 +16,15 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app = Router::new()
-        .route("/", get(|| async {"hello, world!"}));
+    // add CORS
+    let cors = CorsLayer::new().allow_origin(Any);
 
+    // define router paths
+    let app = Router::new()
+        .route("/", get(|| async {"hello, world!"}))
+        .layer(cors);
+
+    // define address and start server
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 9090));
     tracing::debug!("Listening on {}", addr);
     axum::Server::bind(&addr)
